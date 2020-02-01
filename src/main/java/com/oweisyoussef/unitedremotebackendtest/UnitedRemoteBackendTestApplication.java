2506d89a279ad76af65a152f201c7c0dffe07308
Youssef Oweis <oweisyoussef15@gmail.com>
2020-02-01 01:41:59 +0100

    initialize project
    initialize a spring boot project

---
+++ src/main/java/com/oweisyoussef/unitedremotebackendtest/UnitedRemoteBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.oweisyoussef.unitedremotebackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedRemoteBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitedRemoteBackendTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oweisyoussef/unitedremotebackendtest/UnitedRemoteBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oweisyoussef.unitedremotebackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitedRemoteBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
